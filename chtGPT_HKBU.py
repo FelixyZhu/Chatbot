06d6024ae2776745c35e602d5caa4de57e6d0be9
FelixyZhu@example.com <FelixyZhu@example.com>
2024-03-11 19:10:07 +0800

    Fine runned chatGPT_HKBU.py modified

--- chtGPT_HKBU.py
+++ chtGPT_HKBU.py
@@ -2,28 +2,25 @@ import configparser
 import requests
 class HKBU_ChatGPT():
     def __init__(self,config_path='./config.ini'):
-        if type(config_) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config_)
-        elif type(self.config) == configparser.ConfigParser:
-            self.config = config_
-def submit(self,message):
-    conversation = [{"role": "user", "content": message}]
-    url = (self.config['CHATGPT']['BASICURL']) +
-    "/deployments/" + (self.config['CHATGPT']['MODELNAME']) +
-    "/chat/completions/?api-version=" +
-    (self.config['CHATGPT']['APIVERSION'])
+        self.config = configparser.ConfigParser()
+        self.config.read(config_path)
+    def submit(self,message):
+        conversation = [{"role": "user", "content": message}]
 
-    headers = { 'Content-Type': 'application/json',
-    'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
-    payload = { 'messages': conversation }
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['choices'][0]['message']['content']
-    else:
-        return 'Error:', response
+        url = (self.config['CHATGPT']['BASICURL']) +"/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
 
+        headers = { 'Content-Type': 'application/json',
+        'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+
+        payload = { 'messages': conversation }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['choices'][0]['message']['content']
+        else:
+            return 'Error:', response
 
 if __name__ == '__main__':
     ChatGPT_test = HKBU_ChatGPT()
